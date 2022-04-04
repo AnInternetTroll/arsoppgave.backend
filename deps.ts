039ba14cb065ddc933769829280f6e84ea8f7940
AnInternetTroll <lucafulger@gmail.com>
2022-04-04 19:07:14 +0200

    Update dependencies and move from /x/dotenv to /std/dotenv

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 // Handles `.env` file.
 // Technically can be ignored as you can always set env variables yourself.
-import "https://deno.land/x/dotenv@v3.1.0/load.ts";
+import "https://deno.land/std@0.133.0/dotenv/load.ts";
 
 // The ORM library. The way to save all kinds of data
 export {
@@ -23,22 +23,18 @@ export {
 	type Route,
 	Router,
 	Status,
-} from "https://deno.land/x/oak@v10.4.0/mod.ts";
+} from "https://deno.land/x/oak@v10.5.1/mod.ts";
 
 // Utilities
 export {
 	encode as encodeHex,
-} from "https://deno.land/std@0.127.0/encoding/hex.ts";
+} from "https://deno.land/std@0.133.0/encoding/hex.ts";
 export {
 	encode as encodeBase64,
-} from "https://deno.land/std@0.127.0/encoding/base64.ts";
+} from "https://deno.land/std@0.133.0/encoding/base64.ts";
 export {
 	join,
 	normalize,
 	relative,
-} from "https://deno.land/std@0.127.0/path/mod.ts";
-export {
-	Session,
-	SqliteStore,
-} from "https://deno.land/x/oak_sessions@v3.2.3/mod.ts";
-export * as log from "https://deno.land/std@0.126.0/log/mod.ts";
+} from "https://deno.land/std@0.133.0/path/mod.ts";
+export * as log from "https://deno.land/std@0.133.0/log/mod.ts";
---
+++ src/routes/api/rooms/[id].ts
@@ -0,0 +1,34 @@
+import { Status } from "../../../../deps.ts";
+import { authenticate } from "../../../middleware/auth.ts";
+import type { Route } from "../../../middleware/types.d.ts";
+import { Member, Room, User } from "../../../models/mod.ts";
+
+export default {
+	async GET(ctx, next) {
+		const user = await authenticate(ctx);
+		if (!user) return await next();
+		// @ts-ignore what?
+		const room = await Room.find(ctx?.params?.id) as Room;
+		if (!room) return ctx.throw(Status.NotFound);
+		const membersQuery = await Member.where("roomId", "=", room.id).get() as
+			| Member[]
+			| Member;
+		const members = Array.isArray(membersQuery) ? membersQuery : [membersQuery];
+
+		if (user.role === "user") {
+			if (
+				members.filter(
+					(member) => member.id === user.id,
+				)
+			) {
+				return ctx.throw(Status.NotFound);
+			}
+		}
+		ctx.response.headers.set("content-type", "application/json");
+		ctx.response.body = JSON.stringify({
+			id: room.id,
+			owner: room.ownerId,
+			members,
+		});
+	},
+} as Route;
